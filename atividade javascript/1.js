031e18bb0dc83888f47ffb3a4e1b5c5c0a19056c
Gabriel Pedro <130779173+Gabrielh-Pedro@users.noreply.github.com>
2024-04-25 19:49:46 -0300

    Delete atividade javascript directory

--- atividade javascript/1.js
+++
@@ -1,7 +0,0 @@
-let numero1 = 10;
-let numero2 = 100;
-
-alert(`Soma: ${numero1 + numero2}`);
-alert(`Subtração: ${numero2 - numero1}`);
-alert(`Divisão: ${numero2 / numero1}`);
-alert(`Multiplicação: ${numero2 * numero1}`);
--- atividade javascript/3.js
+++
@@ -1,5 +0,0 @@
-let numeroTabuada = 5;
-
-for (let i = 1; i <= 10; i++) {
-  console.log(`${numeroTabuada} x ${i} = ${numeroTabuada * i}`);
-}
